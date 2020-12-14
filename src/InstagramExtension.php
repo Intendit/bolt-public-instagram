b3ca83c62ec5f8371ce71bc273d88180da2db0f6
WeIntendit AB <santino.petrovic@intendit.se>
2020-12-14 10:39:26 +0100

    Fix so the code does not generate error if token is null

--- src/InstagramExtension.php
+++ src/InstagramExtension.php
@@ -24,6 +24,7 @@ class InstagramExtension extends SimpleExtension
             'instagramgraphtoken' => 'instagramGraphTokenTwigFunction',
             'instagramlocationtoken' => 'instagramLocationTokenTwigFunction',
             'instagrammediatokenintendit' => 'instagramMediaTokenIntenditTwigFunction',
+            'instagramgraphtokenbyme' => 'instagramGraphTokenByMeTwigFunction'
         ];
     }
 
@@ -111,20 +112,51 @@ class InstagramExtension extends SimpleExtension
         $config = $this->getConfig();
         // Create a new instagram instance.
         $instagram = new Instagram();
-
+        $path = $_SERVER['DOCUMENT_ROOT'].'/extensions/vendor/santinopetrovic/instagram/';
         // Fetch the media feed.
         if (isset($config['instagram_auth_token'])) {
             $data = $instagram->getGraphToken($config['user_id'], $config['instagram_auth_token'], $limit);
         } else {
             if (file_exists($path.'instagram-temp.json')) {
                 $instagramTemp = json_decode(file_get_contents($path.'instagram-temp.json'));
-                return $data;
+                return $instagramTemp;
+            } else {
+                return array('errorinstagram' => 'Invalid token.');
+            }                        
+        }        
+        if (isset($data) && !isset($data["errorinstagram"]) && is_array($data) && !empty($data)) {
+            // Write temporary content to the file, so if the instagram crashes, the content will be here.
+            file_put_contents($path.'instagram-temp.json', json_encode($data));
+            return $data;
+        } else {
+            if (file_exists($path.'instagram-temp.json')) {
+                $instagramTemp = json_decode(file_get_contents($path.'instagram-temp.json'));
+                return $instagramTemp;
+            } else {
+                return array('errorinstagram' => 'Invalid token.');
+            }
+
+        }
+    }   
+ 
+    public function instagramGraphTokenByMeTwigFunction($limit = 25)
+    {
+        $config = $this->getConfig();
+        // Create a new instagram instance.
+        $instagram = new Instagram();
+        $path = $_SERVER['DOCUMENT_ROOT'].'/extensions/vendor/santinopetrovic/instagram/';
+        // Fetch the media feed.
+        if (isset($config['instagram_auth_token'])) {
+            $data = $instagram->getGraphTokenByMe($config['user_id'], $config['instagram_auth_token'], $limit);
+        } else {
+            if (file_exists($path.'instagram-temp.json')) {
+                $instagramTemp = json_decode(file_get_contents($path.'instagram-temp.json'));
+                return $instagramTemp;
             } else {
                 return array('errorinstagram' => 'Invalid token.');
             }                        
         }
-
-        $path = $_SERVER['DOCUMENT_ROOT'].'/extensions/vendor/santinopetrovic/instagram/';
+        
         if (!isset($data["errorinstagram"]) && isset($data) && is_array($data) && !empty($data)) {
             // Write temporary content to the file, so if the instagram crashes, the content will be here.
             file_put_contents($path.'instagram-temp.json', json_encode($data));
@@ -132,14 +164,14 @@ class InstagramExtension extends SimpleExtension
         } else {
             if (file_exists($path.'instagram-temp.json')) {
                 $instagramTemp = json_decode(file_get_contents($path.'instagram-temp.json'));
-                return $data;
+                return $instagramTemp;
             } else {
-                return $data;
+                return array('errorinstagram' => 'Invalid token.');
             }
 
         }
-    }   
-    
+    }      
+
     public function instagramTagsTokenTwigFunction($tag, $token, $limit)
     {
 
